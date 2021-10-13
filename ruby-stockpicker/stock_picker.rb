4e40b084cd77b98e200aba7bd3c43a8f507807b6
gormlessfie <gmlessfie@gmail.com>
2021-10-13 00:39:07 -0700

    Fixed styling with rubocop

--- ruby-stockpicker/stock_picker.rb
+++ ruby-stockpicker/stock_picker.rb
@@ -1,28 +1,26 @@
+# frozen_string_literal: true
+
 stock_price_per_day = [3, 2, 6, 25, 6, 26, 23, 4, 23, 15, 1, 6, 5]
 
 def stock_picker(input_array)
-    biggest_diff = {
-        diff: 0,
-        index_buy: 0,
-        index_sell: 0,
-    }
+  biggest_diff = {
+    diff: 0,
+    index_buy: 0,
+    index_sell: 0
+  }
 
-    for i in 0...input_array.length do
-        for j in (i+1)...input_array.length do
-            if input_array[i] < input_array[j]
-                if (biggest_diff[:diff] < (input_array[j] - input_array[i]))
+  for i in 0...input_array.length do
+    for j in (i + 1)...input_array.length do
+      next unless input_array[i] < input_array[j]
 
-                    biggest_diff[:diff] = input_array[j] - input_array[i]
-                    biggest_diff[:index_buy] = i
-                    biggest_diff[:index_sell] = j
-                end
-            else
-                next
-            end
+        if biggest_diff[:diff] < (input_array[j] - input_array[i])
+          biggest_diff[:diff] = input_array[j] - input_array[i]
+          biggest_diff[:index_buy] = i
+          biggest_diff[:index_sell] = j
         end
     end
-    biggest_diff
-    puts "Buy on day #{biggest_diff[:index_buy]}, Sell on day #{biggest_diff[:index_sell]}, Profit of $#{biggest_diff[:diff]}"
+  end
+  puts "Buy on day #{biggest_diff[:index_buy]}, Sell on day #{biggest_diff[:index_sell]}, Profit of $#{biggest_diff[:diff]}"
 end
 
 stock_picker(stock_price_per_day)
--- ruby-substrings/substrings.rb
+++ ruby-substrings/substrings.rb
@@ -1,36 +1,44 @@
- dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit", 'even', 'evening']
+# frozen_string_literal: true
 
-
+dictionary = %w[
+  below down go going horn how howdy it i
+  low own part partner sit even evening
+]
 main_input = "Howdy partner, sit down! How's it going?"
 
-def substrings (input, dictionary)
-    split_input = input.downcase.split('').delete_if {|char| char.ord > 'z'.ord || char.ord < 'a'.ord}
-    
-    hash_count = match_characters(split_input, dictionary)
-    puts hash_count
-    hash_count
+def substrings(input, dictionary)
+  split_input = input.downcase.split('').delete_if { |char| char.ord > 'z'.ord || char.ord < 'a'.ord }
+  hash_count = match_characters(split_input, dictionary)
+  puts hash_count
+  hash_count
 end
 
 def match_characters(input, dictionary)
-    matched_words = {}
+  matched_words = {}
+  dictionary.each do |word|
+    increment_count(word, matched_words, input)
+  end
+  matched_words.reject { |_key, value| value.zero? }
+end
 
-    dictionary.each do |word|
-        matched_words["#{word}"] = 0
-        split_word = word.split('')
-        i = 0
+def increment_count(word, matched_words, input)
+  matched_words[word.to_s] = 0
+  split_word = word.split('')
+  check_consecutive(word, input, split_word, matched_words)
+  matched_words
+end
 
-        input.each do |index|
-            if index == split_word[i]
-                i += 1
-                if (i == split_word.length)
-                    matched_words["#{word}"] += 1
-                end
-            else
-                i = 0
-            end
-        end
+def check_consecutive(word, input, split_word, matched_words)
+  i = 0
+  input.each do |index|
+    if index == split_word[i]
+      i += 1
+      matched_words[word.to_s] += 1 if i == split_word.length
+    else
+      i = 0
     end
-    matched_words.select {|key, value| value != 0}
+  end
+  matched_words
 end
 
 substrings(main_input, dictionary)
