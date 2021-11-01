c9cabb328deb6b8b728d92c439933a6f0233d62c
gormlessfie <gmlessfie@gmail.com>
2021-11-01 16:03:35 -0700

    Implemented bfs from a starting square to end square, tracks route and distance

--- ruby-chess-knight/lib/driver.rb
+++ ruby-chess-knight/lib/driver.rb
@@ -4,14 +4,16 @@ require './lib/knight'
 
 knight = Knight.new
 
-puts 'Chess board'
-knight.print_chess_board
+# puts 'Chess board'
+# knight.print_chess_board
 
-puts "\n\n"
+# puts "\n\n"
 
-puts 'List of Possible moves per square'
-knight.print_legal_moves
+# puts 'List of Possible moves per square'
+# knight.print_legal_moves
 
-puts "\n\n"
+# puts "\n\n"
 
+# knight.bfs_start_end([0, 0], [4, 4])
 
+knight.knight_moves([3, 2], [6, 4])
